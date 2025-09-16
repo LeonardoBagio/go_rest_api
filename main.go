5828ab6e7ca66b12f5dd886a5e79a15e433fe732
Leonardo Bagio <leobagio2009@gmail.com>
2025-09-16 16:19:31 -0300

    Add event model and event API endpoints
    Introduces a new Event model in models/event.go with basic in-memory storage. Adds GET and POST /events endpoints to main.go for listing and creating events using Gin, with basic request validation and response handling.

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net/http"
 
+	"example.com/models"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,10 +12,27 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	server.Run(":8080")
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"message": "List of events"})
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Could not parse data."})
+		return
+	}
+	event.ID = 1
+	event.UserID = 1
+
+	event.Save()
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
